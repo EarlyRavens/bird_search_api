62f09c2633d621b4741fb28e5af5230e53134190
Mina Melosh <email": null>
2017-06-12 10:36:10 -0700

    initialize instance of Mechanize

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,7 @@ class SearchController < ApplicationController
     @potential_clients = []
 
     @yelp_businesses = query_yelp_api(params)
+    @mechanize = Mechanize.new
 
     render json: @yelp_businesses.to_json
   end
