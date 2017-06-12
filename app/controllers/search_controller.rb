2a5788e68d5e58447c010af895601771b0ea003c
Mina Melosh <email": null>
2017-06-12 10:35:21 -0700

    render yelp_business json from API call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   include SearchHelper
   def query
+    params[:business] = "korean"
+    params[:location] = "94105"
+    @potential_clients = []
 
+    @yelp_businesses = query_yelp_api(params)
+
+    render json: @yelp_businesses.to_json
   end
 end
