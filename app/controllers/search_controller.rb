bfd9eb39add8e0a5d9eb349fe3016e7a25f4a6a5
Mina Melosh <email": null>
2017-06-12 10:38:48 -0700

    set up formatting for final JSON

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,9 +8,9 @@ class SearchController < ApplicationController
     @yelp_businesses = query_yelp_api(params)
     @mechanize = Mechanize.new
 
-    @yelp_business_threads = @yelp_businesses.map{|business| Thread.new{evaluate(business)}}
-    @yelp_business_threads.each {|thread| thread.join}
+    # @yelp_business_threads = @yelp_businesses.map{|business| Thread.new{evaluate(business)}}
+    # @yelp_business_threads.each {|thread| thread.join}
 
-    render json: @yelp_businesses.to_json
+    render json: {data: @potential_clients.to_json, quote: random}
   end
 end
