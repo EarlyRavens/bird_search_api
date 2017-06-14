f05ddd89757dd7bde72e70806d5d7c05e3c7afbd
Patrick Tangphao <email": null>
2017-06-14 09:58:01 -0700

    make sleep time longer

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,12 +14,13 @@ class SearchController < ApplicationController
     @processing_time = Time.now - start_time
 
     thread1 = Thread.new {
-      sleep 1
+      sleep 5
       p "HEROKU WILL RESTART"
       HTTParty.delete("https://api.heroku.com/apps/threadraven/dynos", headers: {"Authorization" => "Bearer a17504ec-bd05-4f72-8cdb-da9c9a233172", "Accept" => "application/vnd.heroku+json; version=3"})
     }
 
     thread2 = Thread.new {
+      p "RENDER THE JSON"
       render json: {data: @potential_clients, quote: random, processing_time: @processing_time}.to_json}
     thread1.join
     thread2.join
